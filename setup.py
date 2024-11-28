6641523ad1eca1688a3bd1cba27d16c90c961969
aiadebimpe <99357302+AIAdebimpe@users.noreply.github.com>
2024-11-28 14:31:35 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pnflowPy",
-    version="0.1",
+    version="2.1",
     packages=find_packages(),
     install_requires=[],  # Add dependencies if needed
 )
